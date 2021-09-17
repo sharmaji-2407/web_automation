f0d96d0b0b218fcd323f8869bf25b0ce896ab5d1
Prakhar Sharma <40497894+sharmaji-2407@users.noreply.github.com>
2021-09-17 20:29:01 +0530

    Update automation.py

--- automation.py
+++ automation.py
@@ -4,7 +4,7 @@ driver = webdriver.Chrome()
 driver.get('https://youtube.com')
 
 searchbox = driver.find_element_by_xpath('/html/body/ytd-app/div/div/ytd-masthead/div[3]/div[2]/ytd-searchbox/form/div/div[1]/input')
-searchbox.send_keys('Hitesh Choudhary')
+searchbox.send_keys('Github')
 
 searchButton = driver.find_element_by_xpath('/html/body/ytd-app/div/div/ytd-masthead/div[3]/div[2]/ytd-searchbox/form/button')
 searchButton.click()
